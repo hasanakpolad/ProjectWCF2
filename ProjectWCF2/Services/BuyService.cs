f776af1765eaa9f0b31dd3e08a90a51dd13721eb
hasan akpolad <hasanakpolad@hotmail.com>
2020-12-11 22:19:07 +0300

    Add project files.

---
+++ ProjectWCF2/Services/BuyService.cs
@@ -0,0 +1,16 @@
+﻿using ProjectWCF2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectWCF2.Services
+{
+    public class BuyService : IBuyService
+    {
+        public void DoWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectWCF2/Services/CustomerService.cs
@@ -0,0 +1,16 @@
+﻿using ProjectWCF2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectWCF2.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        public void DoWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
